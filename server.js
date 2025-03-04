1c8486fcc55092a5a85eb3fe98650deba849d623
ds-arunkumar <arunkumar519@gmail.com>
2025-03-04 10:02:48 +0530

    custom middleware added

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-const http = require("http");
+// Importing express
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  console.log(`Request made to: ${req.url}`);
-  console.log(`Request method: ${req.method}`);
+// create an express app
+const app = express();
 
-  if (req.url === "/") {
-    if (req.method === "GET") {
-      res.end("GET: Root Route!");
-    } else if (req.method === "POST") {
-      res.end("POST: Root Route!");
-    } else if (req.method === "PUT") {
-      res.end("PUT: Root Route!");
-    } else if (req.method === "DELETE") {
-      res.end("DELETE: Root Route!");
-    } else {
-      res.end("Method Not Allowed");
-    }
-  } else if (req.url === "/users") {
-    res.end("users route");
-  } else {
-    res.end("Route Not Found");
-  }
+// a middleware function to log all requests
+const logger = (req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  console.log(`Request Method: ${req.method}`);
+  console.log(`Request Body: ${req.body}`);
+  console.log(`_________________________`);
+
+  // call the next middleware function in the stack or
+  // pass the request to the next middleware function
+  next();
+};
+
+// use the logger middleware
+app.use(logger);
+
+// request handler
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World!" });
 });
-
-server.listen(3001, "localhost", () => {
-  console.log("Server is running on http://localhost:3001");
+// listen for requests
+app.listen(3001, "localhost", () => {
+  console.log("Server is listening on port 3001");
 });
